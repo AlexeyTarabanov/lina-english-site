00301fa14b92c41aef63a2944deb85724afe0de0
Alexey Tarabanov <alexey.tarabanov@datainstruments.ru>
2026-08-31 15:29:26 +0300

    изменил лого, поправил шрифты

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  build: {
+    cssTarget: "safari14",
+  },
   plugins: [react()],
 });
